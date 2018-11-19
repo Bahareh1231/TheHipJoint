ed4873f871c85b70e3ea032bbb27375d3f7c9cfb
Bahareh <bahareh.adham@gmail.com>
2018-11-19 10:31:52 -0500

    form

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -188,18 +188,18 @@ app.submitForm = function() {
         e.preventDefault();
 
         let $form = $(this);
-        //show some response on the button
-        $('button[type="submit"]', $form).each(function () {
-            let $btn = $(this);
-            $btn.prop('type', 'button');
-            $btn.prop('orig_label', $btn.text());
-            $btn.text('Sending ...');
-        });
-        // after_form_submitted();
+        // //show some response on the button
+        // $('button[type="submit"]', $form).each(function () {
+        //     let $btn = $(this);
+        //     $btn.prop('type', 'button');
+        //     $btn.prop('orig_label', $btn.text());
+        //     $btn.text('Sending ...');
+        // });
+        // // after_form_submitted();
 
         $.ajax({
             type: "POST",
-            url: 'handler.php',
+            url: 'scripts/handler.php',
             data: $form.serialize(),
             success: after_form_submitted(),
             dataType: 'JSON'
